7be4079499bb50fd32895d43bb8e15eee7adc394
Kelvin Quarcoo <kelvinquarcoo2005@gmail.com>
2023-12-01 23:55:50 +0000

    -

--- components/AboutOffice/index.js
+++ components/AboutOffice/index.js
@@ -1,3 +1,5 @@
+import { InterestedForm } from "..";
+
 export default function index() {
   return (
     <div className="mt-16 pr-5 font-Outfit lg:flex lg:gap-20">
@@ -28,32 +30,8 @@ export default function index() {
                 Sed lacinia, massa id porttitor ultrices, sem nisl scelerisque nunc, et condimentum nunc nisl ut arcu. Fusce semper mi pretium lacus viverra ornare vitae eget nibh.
                 </p>
             </div>
-           
         </div>
-        <form action="" className="border px-5 lg:px-10 py-10 rounded-md">
-            <div className="">
-                <h3 className=" text-[#285FE2] text-2xl font-bold mb-3">Interested?</h3>
-                <p className="text-[#242839] text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-            </div>
-            <div className="">
-                <div className="inline-block w-full mt-4">
-                    <label htmlFor="name" className="block mb-2 text-[#7E83AC]">Name</label>
-                    <input type="text"  placeholder="Name" id="name" className="border px-3 py-4 w-full rounded"/>
-                </div>
-                <div className="inline-block w-full mt-4">
-                    <label htmlFor="email" className="block mb-2 text-[#7E83AC]">Email</label>
-                    <input type="text"  placeholder="Email" id="email" className="border px-3 py-4 w-full rounded"/>
-                </div>
-                <div className="inline-block w-full mt-4">
-                    <label htmlFor="phonenumber" className="block mb-2 text-[#7E83AC]">Phone Number</label>
-                    <input type="tel" placeholder="Phone Number" id="phonenumber" className="border px-3 py-4 w-full rounded"/>
-                </div>
-                <button type="submit" className="w-full py-5 bg-[#285FE2] rounded-md text-[#F6F8FF] font-semibold mt-5">
-                    Send Message
-                </button>
-            </div>
-            
-        </form>
+        <InterestedForm />
     </div>
   )
 }
