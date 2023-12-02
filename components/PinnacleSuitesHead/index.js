79fc6e2b0235544c10b3310b4b5bb81cd833fccf
Kelvin Quarcoo <kelvinquarcoo2005@gmail.com>
2023-12-02 22:47:48 +0000

    began progress wwith the single Location

---
+++ components/PinnacleSuitesHead/index.js
@@ -0,0 +1,10 @@
+export default function index() {
+  return (
+    <div>      
+        <section className="pl-5">
+          <h1>Pinnacle Suites</h1>
+          <p>Fusce in aliquet ex, ut efficitur justo. Mauris sit amet feugiat ipsum.</p>
+        </section>      
+    </div>
+  )
+}
---
+++ pages/pinnacle-suite.js
@@ -0,0 +1,13 @@
+import { Navbar, PinnacleSuitesHead } from "../components"
+
+const pinnaclesuite = () => {
+  return (
+        <div>
+          <Navbar />
+            <section className="pl-5">
+             <PinnacleSuitesHead />
+            </section>      
+        </div>
+      )
+  }
+export default pinnaclesuite
