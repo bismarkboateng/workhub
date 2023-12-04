277884d99303b1303479076fbdc7cf1e4652a3a0
Kelvin Quarcoo <kelvinquarcoo2005@gmail.com>
2023-12-04 01:30:21 +0000

    finished with no comments

--- components/MoreLocations/CardsData.js
+++ components/MoreLocations/CardsData.js
@@ -1,10 +1,4 @@
 export const cards = [
-    {   
-        url: "https://assets-global.website-files.com/64ed7cce29f3b2cfe859f6fd/64effc24d6539e7d1c81e84b_Location%201-p-500.jpg",
-        paragraph: "Fusce in aliquet ex, ut efficitur justo. Mauris sit amet feugiat ipsum.",
-        title: "Pinnacle Suits",
-        id:Math.random()
-    },
     { 
         url: "https://assets-global.website-files.com/64ed7cce29f3b2cfe859f6fd/64effbee7bb82584d3fc48fd_Location%205-p-500.jpg",
         paragraph: "Etiam et tellus nibh. Praesent a neque dictum, pretium justo sit amet.",
@@ -18,7 +12,7 @@ export const cards = [
         id:Math.random() 
     },
     { 
-        url: "https://assets-global.website-files.com/64ed7cce29f3b2cfe859f6fd/64effb67871ddc2644b2337b_Location%207-p-500.jpg", 
+        url: "https://assets-global.website-files.com/64ed7cce29f3b2cfe859f6fd/64effa0e7acdc9c29d99a270_Location%2010.jpg", 
         paragraph: "Nunc at pulvinar ligula. Nunc lacinia, metus in facilisis porta, tortor ante.",
         title: "Horizon Haven Offices", 
         id:Math.random() 
