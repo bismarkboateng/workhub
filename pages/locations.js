801a39352c065902eb8d14140e9bff72aeef4fd0
bismarkboateng <bismarkb609@gmail.com>
2023-12-08 16:01:42 +0000

    fixed locations page styling

--- pages/locations.js
+++ pages/locations.js
@@ -1,42 +1,70 @@
-import { Navbar, Footer  } from "../components"
-import { cards } from "../components/Locationcards"
 import Image from 'next/image'
+
+import { Navbar, Footer, Button  } from "../components"
+import { cards } from "../components/Locationcards"
+
+
+
 export default function Locations() {
   return (
-   <div className="text-4xl font-semibold">
+   <div className="">
     <Navbar/>
-    <div className="flex flex-col justify-center items-center text-4xl bg-gray-100 py-20 px-34 gap-y-8">
-      <div className="font-bold">Our Locations</div>
-      <div className="flex flex-col justify-center items-center text-xl text-gray-600">
-      <div className="flex-col justify-center items-center md:justify-start md:items-start">
-       In et lacus sit amet sapien faucibus consectetur.
+
+    <div className="w-[90%] md:w-[95%] 2xl:w-[64%] mx-auto flex flex-col justify-center items-center 
+    bg-[#f0f2fb] py-20 px-34 gap-y-8">
+      <div className="w-[80%] font-semibold font-Outfit text-text-color text-[39px] md:text-[44px]
+       lg:text-[50px] text-center leading-[50.7px]">
+        Our Locations
       </div>
-      <div className="flex justify-center items-center"> mollis, ac bibendum est gravida.</div>
+      <div className="w-[90%] flex flex-col justify-center items-center font-Outfit 
+      text-base md:text-lg text-[#53587a] leading-6 text-center">
+        <div className="flex-col justify-center items-center md:justify-start md:items-start">
+        In et lacus sit amet sapien faucibus consectetur.
+        </div>
+        <div className="flex justify-center items-center"> mollis, ac bibendum est gravida.</div>
       </div>
-      <div className="flex flex-col md:flex-row md:space-x-4 space-y-4 md:space-y-0">
-  <button className="bg-blue-700 text-lg text-white py-2 px-4 rounded">Request a Quote</button>
-  <button className="bg-white text-lg text-blue-950 border-blue-950 border-2 py-2 px-4 rounded">View Locations</button>
-</div>
+      <div className="w-[90%] flex flex-col md:flex-row md:space-x-4 space-y-4 md:space-y-0
+      md:justify-center">
+        <Button
+          className="text-text-color-2 md:text-lg rounded py-[15px] w-[100%] md:w-[30%] font-Outfit px-[23px]
+          lg:w-[23%] 2xl:w-[18%] cursor-pointer outline-none"
+        >
+          Request a Quote
+        </Button>
+        <Button
+          className="font-Outfit text-base md:text-lg text-[#53587a] md:w-[30%] border border-[#53587a]
+          lg:w-[23%] 2xl:w-[18%] bg-[#f0f2fb] outline-none py-[15px] rounded cursor-pointer"
+        >
+          View Locations
+        </Button>
+      </div>
     </div>
 
-{/* cards */}
-<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-20">
-    {cards.map((card) => (
-        <div key={card.id} className="rounded-xl border shadow-sm transform cursor:pointer hover:scale-105 transition-transform duration-200 max-w-[500px]">
-          <Image src={card.url} alt={card.title} width={500} height={300} className="rounded-xl"/>
+    {/* cards */}
+    <div className="w-[90%] md:w-[95%] 2xl:w-[64%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8
+      mt-20">
+      {cards.map((card) => (
+        <div key={card.id} className="rounded-xl border shadow-sm transform cursor:pointer 
+          hover:scale-105 transition-transform duration-200 max-w-[500px]">
+          <Image src={card.url} alt={card.title} width={500} height={300} className=""/>
           <div className="p-4">
-          <h2 className="text-2xl pb-8 transition-colors duration-200 hover:text-blue-700">{card.title}</h2>
-          <p className="text-lg pb-12 text-gray-600">{card.paragraph}</p>
-          <p className="text-blue-700 font-bold text-lg">Learn More</p>
+            <h2 className="text-xl md:text-[23px] font-Outfit text-text-color font-semibold leading-[26px]
+            transition-colors duration-200 hover:text-blue-700 mb-3">
+              {card.title}
+            </h2>
+            <p className="text-base md:text-lg font-Outfit text-[#53587a] mb-3 leading-6">
+              {card.paragraph}
+            </p>
+            <p className="text-base md:text-lg font-Outfit text-text-color-3 font-semibold
+            leading-[20.8px] mb-3">
+              Learn More
+            </p>
           </div>
         </div>
       ))} 
-</div>
-
-
-
-
-   </div>
+    </div>
+    <Footer />
+  </div>
   )
 }
 
